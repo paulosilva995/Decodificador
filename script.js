19360b64db0bb9514d9cd900a175e9bbce2df587
Paulo_Silva_EngSoft <125996306+paulosilva995@users.noreply.github.com>
2024-03-11 21:15:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,102 @@
+var botaoCriptografar = document.querySelector(".btn-criptografar");
+var botaoDescriptografar = document.querySelector(".btn-descriptografar");
+var robo = document.querySelector(".contemrobo");
+var contem = document.querySelector(".contem-paragrafo");
+var resultado = document.querySelector(".texto-resultado");
+
+botaoCriptografar.onclick = criptografar;
+botaoDescriptografar.onclick = descriptografar;
+
+function alterarTema(opcao) {
+    var body = document.body;
+
+    if (opcao === 'OS Default') {
+        body.classList.remove('light-mode', 'dark-mode');
+    } else {
+        body.classList.remove('light-mode', 'dark-mode');
+        body.classList.add(opcao.toLowerCase() + '-mode');
+    }
+
+    salvarModo(opcao.toLowerCase());
+}
+
+function criptografar() {
+    ocultar();
+    var caixadetexto = recuperarTexto();
+    resultado.textContent = criptografarTexto(caixadetexto);
+}
+
+function descriptografar() {
+    ocultar();
+    var caixadetexto = recuperarTexto();
+    resultado.textContent = descriptografarTexto(caixadetexto);
+}
+
+function recuperarTexto() {
+    var caixadetexto = document.querySelector(".caixadetexto");
+    return caixadetexto.value;
+}
+
+function ocultar() {
+    robo.classList.add("ocultar");
+    contem.classList.add("ocultar");
+}
+
+function criptografarTexto(mensagem) {
+    var texto = mensagem;
+    var textoFinal = "";
+
+    for (var i = 0; i < texto.length; i++) {
+        if (texto[i] == "a") {
+            textoFinal = textoFinal + "ai";
+        } else if (texto[i] == "e") {
+            textoFinal = textoFinal + "enter";
+        } else if (texto[i] == "i") {
+            textoFinal = textoFinal + "imes";
+        } else if (texto[i] == "o") {
+            textoFinal = textoFinal + "ober";
+        } else if (texto[i] == "u") {
+            textoFinal = textoFinal + "ufat";
+        } else {
+            textoFinal = textoFinal + texto[i];
+        }
+    }
+    return textoFinal;
+}
+
+function descriptografarTexto(mensagem) {
+    var texto = mensagem;
+    var textoFinal = "";
+
+    for (var i = 0; i < texto.length; i++) {
+        if (texto[i] == "a") {
+            textoFinal = textoFinal + "a";
+            i = i + 1;
+        } else if (texto[i] == "e") {
+            textoFinal = textoFinal + "e";
+            i = i + 4;
+        } else if (texto[i] == "i") {
+            textoFinal = textoFinal + "i";
+            i = i + 3;
+        } else if (texto[i] == "o") {
+            textoFinal = textoFinal + "o";
+            i = i + 3;
+        } else if (texto[i] == "u") {
+            textoFinal = textoFinal + "u";
+            i = i + 3;
+        } else {
+            textoFinal = textoFinal + texto[i];
+        }
+    }
+
+    return textoFinal;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const btnCopiar = document.querySelector(".btn-copiar");
+    btnCopiar.addEventListener("click", () => {
+        var contido = document.querySelector(".texto-resultado").textContent;
+        navigator.clipboard.writeText(contido);
+        console.log("Olá");
+    });
+});
